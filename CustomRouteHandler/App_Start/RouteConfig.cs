f9960ba7c6a4c4494815b35aecfd570f0f847b8d
Ashish Suman <ashishsuman4u@gmail.com>
2015-07-04 02:26:55 -0400

    Initial

---
+++ CustomRouteHandler/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CustomRouteHandler
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteValueDictionary defaults = new RouteValueDictionary();
+            defaults.Add("controller", "Home");
+            defaults.Add("action", "Index");
+            defaults.Add("id", "");
+            var customRoute = new Route("{controller}/{action}/{id}", defaults, new RouteHandler.CustomRouteHandler());
+            routes.Add(customRoute);
+        }
+    }
+}
---
+++ CustomRouteHandler/RouteHandler/CustomRouteHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace CustomRouteHandler.RouteHandler
+{
+    public class CustomRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new CustomHandler(requestContext);
+        }
+    }
+}
